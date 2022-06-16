2153c71ae2d5b5cd9f0c6446485eb14daa78e13a
Tauseef Ahmad <tousifahmad57@gmail.com>
2022-06-16 15:10:02 +0500

    initial commit

--- app/src/main/java/pk/mosafir/travsol/ui/MainActivity.kt
+++ app/src/main/java/pk/mosafir/travsol/ui/MainActivity.kt
@@ -50,6 +50,7 @@ class MainActivity : AppCompatActivity() {
         lateinit var firebaseToken: String
         var userId = 0L
     }
+    //
 /////
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
